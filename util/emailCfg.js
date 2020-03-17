b0dc3e3a5ec73d13175e3d85fd003063fe949500
丘桂娴 <793035324@qq.com>
2020-03-17 10:35:40 +0800

    feat('邮箱的更改')

--- util/emailCfg.js
+++ util/emailCfg.js
@@ -3,30 +3,30 @@ const user_email = "793035324@qq.com";
 const auth_code = "jxcptqqnluywbbhh";
 
 const transporter = nodemailer.createTransport({
-    service: "qq",
-    secureConnection: true,
-    port: 465,
-    auth: {
-      user: user_email, // 用户邮箱
-      pass: auth_code // 授权码
-    }
-  });
-
-  const emailCfg= {
-    async sendMail(email, subject, text, html) {
-        const mailOptions = {
-          from: user_email, // 发送者,与上面的user一致
-          to: email, // 接收者,可以同时发送多个,以逗号隔开
-          subject, // 标题
-          text, // 文本
-          html
-        };
-        try {
-          await transporter.sendMail(mailOptions);
-          return true;
-        } catch (err) {
-          return false;
-        }
-      }
+  service: "qq",
+  secureConnection: true,
+  port: 465,
+  auth: {
+    user: user_email, // 用户邮箱
+    pass: auth_code // 授权码
   }
-module.exports=emailCfg
+});
+
+const emailCfg = {
+  sendMail(email, subject, text, html) {
+    const mailOptions = {
+      from: user_email, // 发送者,与上面的user一致
+      to: email, // 接收者,可以同时发送多个,以逗号隔开
+      subject, // 标题
+      text, // 文本
+      html
+    };
+    try {
+      transporter.sendMail(mailOptions);
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+}
+module.exports = emailCfg
